46b6f93eb657e1cfeb5de9328b1d418b7f023fe3
Christopher Aram Swayne <chris_swa@icloud.com>
2019-11-21 16:09:31 -0600

    Modified README.md
    Updated the README.md to include a link to a contributor.
    
    The remaining changes to Devices.cs were to redesign the application.

--- NameGenerator/NameGenerator/Devices.cs
+++ NameGenerator/NameGenerator/Devices.cs
@@ -4,34 +4,34 @@ namespace NameGenerator
 
     public enum Vehicle { Cruiser, Van, Truck, Intercepter }
 
+    /// <summary>
+    /// List of ranked processors. If your processor isn't in this list, feel
+    /// free to add it. Items at the beginning are lowest ranked, whilst items
+    /// at the end are highest ranked.
+    /// i.e. Core Duo is a very low (poor) ranked processor, while Core i9 is a
+    /// very high ranked processor.
+    /// </summary>
+    public enum Core
+    {
+        Duo, Duo2, ARM, ARM32, ARM64, A4, A6, A8, Ryzen3, i3, A10, Ryzen5, i5,
+        Ryzen7, i7, Ryzen9, i9
+    }
+     
+
     public class Devices
     {
 
+        public string Name { get; private set; }
         public string Type { get; private set; }
-        public string Association { get; private set; }
+        public Core Processor;
+        public int Age { get; private set; }
+        public int RAM { get; private set; }
+        
         public Vehicle vehicle;
 
-        public Devices(string t, string a, int power)
+        public Devices()
         {
-            Type = t;
-            Association = a;
-            switch (power) {
-                case 1:
-                    vehicle = Vehicle.Cruiser;
-                    break;
-                case 2:
-                    vehicle = Vehicle.Van;
-                    break;
-                case 3:
-                    vehicle = Vehicle.Truck;
-                    break;
-                case 4:
-                    vehicle = Vehicle.Intercepter;
-                    break;
-                default:
-                    vehicle = Vehicle.Cruiser;
-                    break;
-            }
+
         }
 
     }
