f0e873fa8c930181bae18f515e236810778533ca
Christopher Aram Swayne <chris_swa@icloud.com>
2019-11-21 21:59:43 -0600

    Added more test cases
    Tested with the creation of more devices.

--- NameGenerator/NameGenerator/Program.cs
+++ NameGenerator/NameGenerator/Program.cs
@@ -12,10 +12,17 @@ namespace NameGenerator
                 " the name generator customized to fit your own themes or choices.");
 
             Device MacBookPro = new Device("MacBook Pro", "laptop", Core.i5, 2015, 8);
+            Device DellXPS = new Device("Dell XPS 13", "laptop", Core.i7, 2016, 32);
+            Device HomeDesktop = new Device("Christopher-PC", "desktop", Core.A8, 2013, 24);
+
             Generator generator = new Generator();
             
 
             Console.WriteLine(generator.createName(MacBookPro));
+            Console.WriteLine(generator.createName(DellXPS));
+            Console.WriteLine(generator.createName(HomeDesktop));
+
+
             Console.ReadKey();
         }
     }
